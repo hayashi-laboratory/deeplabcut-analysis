a7b18927c92106a70c9251a18e6898162e36938c
Takaya-s <59642394+Takaya-s@users.noreply.github.com>
2022-04-13 21:45:43 +0900

    Update analysis.py
    Added "videofilename" key on OrderedDict at summary_df.

--- src/pyDLCbehavior/analysis.py
+++ src/pyDLCbehavior/analysis.py
@@ -636,6 +636,7 @@ class YMazeAnalysis(DLCDataset):
     def summary_df(self) -> pd.DataFrame:
         return pd.DataFrame.from_dict(
             OrderedDict(
+                videofilename = [self.csv_path.stem.split("_")[0],
                 filename=[self.csv_path.stem],
                 video_shape=[self.frame_dimensions],
                 video_fps=[self.FPS],
